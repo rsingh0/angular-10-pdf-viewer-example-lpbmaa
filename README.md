0ce3d5c34f8c452c3e6bafa075b4f0cb12abfb35
rsingh0 <robinapr141989@gmail.com>
2021-12-03 02:26:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-10-pdf-viewer-example-lpbmaa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-10-pdf-viewer-example-lpbmaa)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+   pdfSource = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+}
